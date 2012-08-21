4b17fe3ee1588976c4d5a3f74f3e17cbffc181e5
Cristian Mircea Messel <cristian.messel@smarttech2000.eu>
2012-08-21 17:11:55 +0300

    foobar

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReparClujul::Application.config.secret_token = 'e8a9196a198e78390936c1402841a5e0c9ccab7121b2ed509d9e77b90282197c90c679f05e2f78d68aeb37a94a0c9ce20db03d9f8876f01977843ebc76e94e86'
