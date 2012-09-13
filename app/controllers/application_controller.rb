86fb6f4f01a115306b2e430430db9b47d219daf2
Cristian Ilea <cristian.ilea@smarttech2000.com>
2012-09-13 11:52:51 +0300

    implemented method to generate standard API response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def render_response code, message, response_hash
+    {
+      "status" =>
+       {
+          "code" => code,
+          "message" => message
+       },
+      "response" => response_hash
+    }
+  end
 end
