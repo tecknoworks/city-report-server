a6e2cab987b460f4b610657f42369d0af2569c18
Cristian Ilea <cristian.ilea@smarttech2000.com>
2012-09-19 15:34:50 +0300

    added documentation to categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,4 @@
 class CategoriesController < ApplicationController
-  respond_to :json, :xml
-
   api :GET, "/categories", "Show all categories"
   description "returns a JSON containing all the categories available"
   formats ['json']
@@ -17,12 +15,40 @@ class CategoriesController < ApplicationController
         ]
     }
 }"
-  see "categories#index"
   def index
     categories = Category.all
-    respond_with render_response(ApiStatus.OK_CODE, ApiStatus.OK, {categories:categories})
+    respond_to do |format|
+      format.json { render :json => render_response(ApiStatus.OK_CODE, ApiStatus.OK, {categories:categories}) }
+    end
   end
 
+  api :POST, "/categories", "Create one category"
+  error :code => ApiStatus.BAD_REQUEST_CODE, :desc => ApiStatus.BAD_REQUEST
+  param :name, String, :desc => "Name of the new category", :required => true
+  description "Creates a category on succes; return JSON with status code succes or bad request. Also, returns the created category on success."
+  formats ['json']
+  example "{ 
+    'status':{
+      'code':200,
+      'message':'Success'
+    }, 
+    'response': {
+      'category': {
+          'id': 132,
+          'name': 'groapa',
+          'updated_at': '2012-09-13 15:14:33',
+          'created_at': '2012-09-13 15:06:33'
+        }
+    }
+}"
+  example "{ 
+    'status':{
+      'code': 400,
+      'message':'Bad Request'
+    }, 
+    'response': {
+    }
+}"
   def create
     if !params[:category].nil? && !params[:category][:name].nil?
       @category = Category.new(params[:category])
@@ -37,6 +63,36 @@ class CategoriesController < ApplicationController
     end
   end
 
+  api :PUT, "/category/:id", "Edit one category"
+  error :code => ApiStatus.BAD_REQUEST_CODE, :desc => ApiStatus.BAD_REQUEST
+  error :code => ApiStatus.NOT_FOUND_CODE, :desc => ApiStatus.NOT_FOUND
+  param :name, String, :desc => "new name", :required => true
+  description "Edits a category on succes; return JSON with status code succes or bad request."
+  formats ['json']
+  example "{ 
+    'status':{
+      'code':200,
+      'message':'Success'
+    }, 
+    'response': {
+    }
+}"
+  example "{ 
+    'status':{
+      'code': 400,
+      'message':'Bad Request'
+    }, 
+    'response': {
+    }
+}"
+  example "{ 
+    'status':{
+      'code':404,
+      'message':'Not Found'
+    }, 
+    'response': {
+    }
+}"
   def update
     if Category.exists?(params[:id])
       if !params[:category].nil? && !params[:category][:name].nil?
@@ -58,6 +114,26 @@ class CategoriesController < ApplicationController
     end
   end
 
+  api :DELETE, "/category/:id", "Delete one category"
+  error :code => ApiStatus.BAD_REQUEST_CODE, :desc => ApiStatus.BAD_REQUEST
+  description "Deletes a category on succes; return JSON with status code succes or bad request."
+  formats ['json']
+  example "{ 
+    'status':{
+      'code':200,
+      'message':'Success'
+    }, 
+    'response': {
+    }
+}"
+  example "{ 
+    'status':{
+      'code': 404,
+      'message':'Not Found'
+    }, 
+    'response': {
+    }
+}"
   def destroy
     if Category.exists?(params[:id])
       Category.delete(params[:id])
