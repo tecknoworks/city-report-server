411fce3f6ad40b0565799c464ec4e5842fd9db12
Cristian Mircea Messel <cristian.messel@smarttech2000.eu>
2012-09-20 10:58:38 +0300

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,6 @@
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#    Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create(name: 'Gropi')
