80032a86a66d99ae3325f7042c15377059ce5b8d
Cristian Ilea <cristian.ilea@smarttech2000.com>
2012-10-18 16:50:55 +0300

    implemented uploading image functionality

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -3,7 +3,22 @@ class AttachmentsController < ApplicationController
     @attachment = Attachment.new
   end
 
+  # this is only for TESTING purposes
   def create
-    @attachment = Attachment.create(params[:attachment])
+    unless params[:image].blank?
+      temp = params[:image]
+      File.open(params[:image_file_name],"wb") do |file|
+        file.write(ActiveSupport::Base64.decode64(temp))
+      end
+      puts params[:image]
+      f = File.open(params[:image_file_name])
+      @issue = Issue.find(params[:issue_id])
+      @attachment = Attachment.new
+      @attachment.image = f
+      @attachment.save
+      @issue.attachment = @attachment
+      File.delete(params[:image_file_name])
+      render :json => render_response(ApiStatus.OK_CODE, ApiStatus.OK, {issue:@attachment})
+    end
   end
 end
