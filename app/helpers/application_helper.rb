23e33e3f5becf14974e95df1488dccff5bfd55c7
Cristian Mircea Messel <cristian.messel@smarttech2000.eu>
2012-11-13 13:07:02 +0200

    remove useless files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,5 @@
 module ApplicationHelper
 
-  def menu_items
-    items = {
-              "documentatie" => apipie_path,
-            }
-
-    result = ""
-
-    items.each do |item_name, url|
-      if current_uri == url
-        result += "<li class='active'><a href='#{url}'>#{item_name.capitalize}</a></li>"
-      else
-        result += "<li><a href='#{url}'>#{item_name.capitalize}</a></li>"
-      end
-    end
-
-    return result.html_safe
-  end
-
-
   def dropdown_list items
     result = "" 
 
--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,6 +1,6 @@
 Apipie.configure do |config|
   config.app_name = "Repara Clujul"
-  config.copyright = "&copy; 2012 SmartTech2000"
+  config.copyright = "MIT"
   config.doc_base_url = "/apidoc"
   config.api_base_url = ""
   config.validate = false
@@ -8,8 +8,6 @@ Apipie.configure do |config|
   config.reload_controllers = true
   config.api_controllers_matcher = File.join(Rails.root, "app", "controllers", "**","*.rb")
   config.app_info = <<-DOC
-    This is the "repara clujul" API documentation. The API is in a relatively primitive state
-    but we hope to make great progress in time. If you find any issues, please contact one of
-    the developers (ex: cristian.ilea [at] smarttech2000.com ).
+    This is the "repara clujul" API documentation.
   DOC
 end
