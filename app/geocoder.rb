f7921f191f01a20149721343dc7ba70aef45ae37
Cristian Messel <mess110@gmail.com>
2013-04-18 19:40:03 +0300

    geocoding works

--- app/geocoder.rb
+++ app/geocoder.rb
@@ -2,13 +2,14 @@ class Geocoder
   include HTTParty
   base_uri 'maps.googleapis.com'
 
+  # TODO think of a good name
   def self.kung_foo lat, lon
-    options = {
-      :query => {
-        :latLng => "#{lat},#{lon}",
-        :sensor => false
-      }
-    }
-    get("/maps/api/geocode/json", options).body
+    geo = get("/maps/api/geocode/json?latlng=#{lat},#{lon}&sensor=false").body
+    json = JSON.parse(geo)
+    return '' if json['status'] != 'OK'
+
+    return json['results'][0]['formatted_address']
+  rescue
+    return ''
   end
 end
--- spec/app/geocoder_spec.rb
+++ spec/app/geocoder_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe Geocoder do
+  # TODO write a real test
   it "should geocode stuff" do
-    Geocoder.kung_foo 46.7569358, 23.597322
+    geo = Geocoder.kung_foo 46.7569358, 23.597322
+    geo.should == 'Calea Turzii, Cluj-Napoca, Romania'
   end
 end
