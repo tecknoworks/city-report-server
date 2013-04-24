8a8f1dc341de3b33b53b35edd35c0c43322088e2
Cristian Mircea Messel <cristian.messel@smarttech2000.eu>
2013-04-24 13:09:14 +0300

    add analytics

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+var prepareToggleButton = function () {
   $('#map-toggle').click(function() {
     $('#map-overlay').animate({
       height: 'toggle'
@@ -6,7 +6,9 @@ $(document).ready(function() {
       $('#map-toggle img').toggleClass('rotate180');
     });
   });
+}
 
+var initMap = function () {
   var mapDiv = $('#map-main')[0];
   var mapCenter = new google.maps.LatLng(46.768322, 23.595002);
   var mapOptions = {
@@ -16,60 +18,66 @@ $(document).ready(function() {
     scrollwheel: true,
     disableDefaultUI: true
   }
-  var map = new google.maps.Map(mapDiv, mapOptions);
+  return new google.maps.Map(mapDiv, mapOptions);
+}
+
+var getMarkerIcon = function (issue) {
+  if (issue.categories === undefined) {
+    return '/images/icons/altele.png';
+  }
+  if (issue.categories[0] === undefined) {
+    return '/images/icons/altele.png';
+  }
+  switch (issue.categories[0]) {
+    case 'gunoi':
+      return '/images/icons/gunoi.png';
+    case 'groapa':
+      return '/images/icons/groapa.png';
+    case 'rutiere':
+      return '/images/icons/rutiere.png';
+    case 'vandalism':
+      return '/images/icons/vandalism.png';
+    default:
+      return '/images/icons/altele.png';
+  }
+}
+
+var issueToPopup = function (issue) {
+
+  var infoWindowContent = $("<div>");
+  var title = $("<h3>").html(issue['title']);
+  infoWindowContent.append(title);
+
+  if (issue.hasOwnProperty('images')) {
+    if (issue['images'].length > 0) {
+      var img = $("<img>").attr('src', issue['images'][0]).addClass('small');
+      infoWindowContent.append(img);
+    }
+  }
+
+  var lat = $("<p>").html(issue['lat'] + " - " + issue['lon']);
+  infoWindowContent.append(lat);
+
+  if (issue.hasOwnProperty('address')) {
+    var address = $("<p>").html(issue['address']);
+    infoWindowContent.append(address);
+  }
+
+  if (issue.hasOwnProperty('description')) {
+    var description = $("<p>").html(issue['description']);
+    infoWindowContent.append(description);
+  }
+
+  return infoWindowContent.html();
+}
+
+$(document).ready(function() {
+  prepareToggleButton();
+  var map = initMap();
 
   google.maps.event.addListener(map, 'click', function(data) {
     console.log(data.latLng);
   });
-
-  var getMarkerIcon = function (issue) {
-    if (issue.categories === undefined) {
-      return '/images/icons/altele.png';
-    }
-    if (issue.categories[0] === undefined) {
-      return '/images/icons/altele.png';
-    }
-    switch (issue.categories[0]) {
-      case 'gunoi':
-        return '/images/icons/gunoi.png';
-      case 'groapa':
-        return '/images/icons/groapa.png';
-      case 'rutiere':
-        return '/images/icons/rutiere.png';
-      case 'vandalism':
-        return '/images/icons/vandalism.png';
-      default:
-        return '/images/icons/altele.png';
-    }
-  }
-  
-  var issueToPopup = function (issue) {
-    var infoWindowContent = $("<div>");
-    var title = $("<h3>").html(issue['title']);
-    infoWindowContent.append(title);
-  
-    if (issue.hasOwnProperty('images')) {
-      if (issue['images'].length > 0) {
-        var img = $("<img>").attr('src', issue['images'][0]).addClass('small');
-        infoWindowContent.append(img);
-      }
-    }
-
-    var lat = $("<p>").html(issue['lat'] + " - " + issue['lon']);
-    infoWindowContent.append(lat);
-  
-    if (issue.hasOwnProperty('address')) {
-      var address = $("<p>").html(issue['address']);
-      infoWindowContent.append(address);
-    }
-
-    if (issue.hasOwnProperty('description')) {
-      var description = $("<p>").html(issue['description']);
-      infoWindowContent.append(description);
-    }
-
-    return infoWindowContent.html();
-  }
 
   $.get('/issues', function(data) {
     for (var pin in data) {
