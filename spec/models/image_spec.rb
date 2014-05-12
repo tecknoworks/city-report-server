b937870de43f35901e59422985451434ba778732
Cristian Mircea Messel <cristian.messel@tecknoworks.com>
2014-05-12 13:51:50 +0300

    tests for Image#to_api method

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -11,4 +11,11 @@ describe Image do
     i.url.should_not be_nil
     i.thumb_url.should_not be_nil
   end
+
+  it 'only displays relevant info when using to_api' do
+    api_keys = Image.create(original_filename: 'foo.png').to_api.keys
+    api_keys.length.should == 2
+    api_keys.include?(:url).should be_true
+    api_keys.include?(:thumb_url).should be_true
+  end
 end
