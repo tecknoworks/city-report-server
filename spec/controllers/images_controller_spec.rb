13affc6ea262e63421cf2e972817dbab8679c35a
Cristian Mircea Messel <cristian.messel@tecknoworks.com>
2014-05-12 16:16:15 +0300

    tests for ImagesController

--- spec/controllers/images_controller_spec.rb
+++ spec/controllers/images_controller_spec.rb
@@ -4,6 +4,26 @@ describe ImagesController do
   let(:filename) { 'test.png' }
   let(:non_png_filename) { 'test.tar.gz' }
 
+  it 'keep track of uploaded images in the database' do
+    Image.any_instance.stub(:storage_filename).and_return(filename)
+    expect {
+      post '/', { 'image' => Rack::Test::UploadedFile.new('spec/assets/logo.png', 'image/png') }
+    }.to change { Image.count }.by 1
+  end
+
+  it 'should show the url and thumb_url' do
+    Image.any_instance.stub(:storage_filename).and_return(filename)
+    post '/', { 'image' => Rack::Test::UploadedFile.new('spec/assets/logo.png', 'image/png') }
+
+    image = JSON.parse(last_response.body)
+    image['body'].should_not be_nil
+    last_response.status.should == 200
+    image = image['body']
+    image['_id'].should be_nil
+    image['url'].should_not be_nil
+    image['thumb_url'].should_not be_nil
+  end
+
   it 'should upload files' do
     Image.any_instance.stub(:storage_filename).and_return(filename)
 
