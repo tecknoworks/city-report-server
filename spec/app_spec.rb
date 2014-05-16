4257a4dcafa07bdcf13c2aa0359794a949fd72e2
Cristian Mircea Messel <cristian.messel@tecknoworks.com>
2014-05-16 10:38:34 +0300

    add app/app test

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -4,4 +4,9 @@ describe Repara do
   it 'knows it\'s running in the test env' do
     Repara.config['env'].should == 'test'
   end
+
+  # used in tests
+  it 'has at least 3 categories' do
+    Repara.categories.count >= 3
+  end
 end
