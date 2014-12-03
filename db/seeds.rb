19c8603b6d5c2b4c5e2398146c1265394675f1e9
Cristian Mircea Messel <mess110@gmail.com>
2014-12-04 00:51:53 +0200

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+['groapă', 'gunoi', 'vandalism', 'altele'].each do |cat|
+  Category.where(name: cat).first_or_create
+end
+
+[
+  'Andrei Mureșanu',
+  'Bulgaria',
+  'Bună Ziua',
+  'Centru',
+  'Dâmbul Rotund',
+  'Gara',
+  'Gheorgheni',
+  'Grădini Mănăștur',
+  'Grigorescu',
+  'Gruia',
+  'Hidelve',
+  'Iris',
+  'Între Lacuri',
+  'Mănăștur',
+  'Mărăști',
+  'Someșeni',
+  'Zorilor',
+  'Sopor',
+  'Borhanci',
+  'Becaș',
+  'Făget',
+  'Zorilor sud (Europa)',
+  'Lomb',
+  'Tineretului',
+  'Pata-Rât'
+].each do |zone|
+  Zone.where(name: zone).first_or_create
+end
