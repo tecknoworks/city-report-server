fec3122a54ff0e8f54fde8e087b8b0bab196325b
Cristian Mircea Messel <cristian.messel@tecknoworks.com>
2015-01-23 12:19:03 +0200

    test for checking the correct address format

--- spec/models/banned_ip_spec.rb
+++ spec/models/banned_ip_spec.rb
@@ -11,4 +11,34 @@ describe BannedIp do
     banned_ip = build :banned_ip, address: nil
     expect(banned_ip).to_not be_valid
   end
+
+  it 'validates the format of the address' do
+    banned_ip = build :banned_ip, address: 'hello'
+    expect(banned_ip).to_not be_valid
+
+    banned_ip = build :banned_ip, address: ''
+    expect(banned_ip).to_not be_valid
+
+    banned_ip = build :banned_ip, address: '299.299.299.299'
+    expect(banned_ip).to_not be_valid
+
+    banned_ip = build :banned_ip, address: 'google.com'
+    expect(banned_ip).to_not be_valid
+
+    banned_ip = build :banned_ip, address: '129.168.0.'
+    expect(banned_ip).to_not be_valid
+
+    banned_ip = build :banned_ip, address: '129.168.0'
+    expect(banned_ip).to_not be_valid
+
+
+    banned_ip = build :banned_ip, address: '192.168.0.19'
+    expect(banned_ip).to be_valid
+
+    banned_ip = build :banned_ip, address: '192.168.200.192'
+    expect(banned_ip).to be_valid
+
+    banned_ip = build :banned_ip, address: '1.1.2.1'
+    expect(banned_ip).to be_valid
+  end
 end
