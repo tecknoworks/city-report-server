7d8af27e043b77801079cfed96385b893c8629ee
Ionut Muresan <muresan.ionut95@gmail.com>
2015-01-27 16:52:16 +0200

    Validate address

--- app/models/banned_ip.rb
+++ app/models/banned_ip.rb
@@ -1,5 +1,6 @@
 # CODE: Remove all whitespace
 class BannedIp < BaseModel
+  require "resolv"
   
   self.primary_key = 'id'
   
@@ -13,26 +14,8 @@ class BannedIp < BaseModel
   protected
   
   def ip_format
-    ip = []
-    if address
-      ip = address.split('.')
-    end
-
-    self.errors.add(:invalide_ip_format, 'ip invalid') if ip.count != 4
-
-    for i in ip
-      self.errors.add(:invalide_ip_format, 'ip invalid') if i.to_i<0 or i.to_i > 255
-    end
-
-    # what happend to the present? method
-    def numeric?(number)
-      Integer(number) != nil rescue false
-    end
-
-    for num in ip
-      self.errors.add(:invalide_ip_format, 'ip invalid') if !numeric?(num)
-    end
-
+    
+    self.errors.add(:invalide_ip_format, 'ip invalid ') if !(address =~ Resolv::IPv4::Regex ? true : false )
     self.errors.add(:invalide_ip_format, 'ip invalid ') if Issue.where(address: address).count == 1
 
   end
