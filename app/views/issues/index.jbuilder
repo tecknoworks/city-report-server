45de72e509587447de0fd9b0d6f631df68eeb583
Cristian Mircea Messel <cristian.messel@tecknoworks.com>
2015-01-27 11:37:55 +0200

    add banned IP tests

--- app/views/issues/index.jbuilder
+++ app/views/issues/index.jbuilder
@@ -1,4 +1,4 @@
-json.code @status_code || 200
+json.code @status_code || RequestCodes::SUCCESS
 json.body do |json|
   json.partial! 'issue', collection: @issues, as: :issue
 end
--- spec/controllers/banned_ips_controller_spec.rb
+++ spec/controllers/banned_ips_controller_spec.rb
@@ -1,8 +1,29 @@
 require 'spec_helper'
 
 describe BannedIpsController, type: :controller do
+  before :each do
+    create :banned_ip
+    create :banned_ip
+  end
+
   it 'works' do
     get :index
     response.status.should eq RequestCodes::SUCCESS
+  end
+
+  it 'has the code and body params in the result' do
+    get :index
+    expect(json.key?('code')).to eq true
+    expect(json.key?('body')).to eq true
+  end
+
+  it 'has status code 200' do
+    get :index
+    expect(json['code']).to eq RequestCodes::SUCCESS
+  end
+
+  it 'returns the two banned ips' do
+    get :index
+    expect(json['body'].class).to be Array
   end
 end
