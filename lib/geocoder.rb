15157c298100d82feb0d8e1a261da601229e4d60
Ionut Muresan <muresan.ionut95@gmail.com>
2015-02-27 13:43:45 +0200

    create function get_info_zone for Geocoder class

--- lib/geocoder.rb
+++ lib/geocoder.rb
@@ -2,13 +2,18 @@
 class Geocoder
   include HTTParty
   base_uri 'maps.googleapis.com'
+  
+  def self.get_info_zone(lat, lon)
+    geo = get("/maps/api/geocode/json?latlng=#{lat},#{lon}&sensor=false").body
+    json = JSON.parse(geo)
+    return json
+  end
 
   # TODO: think of a good name
   def self.kung_foo(lat, lon)
     return '' if lat.to_f == 0.0 || lon.to_f == 0.0
 
-    geo = get("/maps/api/geocode/json?latlng=#{lat},#{lon}&sensor=false").body
-    json = JSON.parse(geo)
+    json = self.get_info_zone(lat, lon)
     return '' if json['status'] != 'OK'
 
     return json['results'][0]['formatted_address']
@@ -16,3 +21,5 @@ class Geocoder
     return ''
   end
 end
+
+#verif in ce cartier este un issue
--- spec/lib/geocoder_spec.rb
+++ spec/lib/geocoder_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe Geocoder do
 
+  it 'return info zone' do 
+    expect(Geocoder.get_info_zone(46.76714653871027, 23.586959838867188)['results'][0]['address_components'][1]['long_name'].to_s).to eq "Strada Republicii"
+  end
+
   it 'return locality' do
     expect(Geocoder.kung_foo(46.777331119287105, 23.614468574523926).to_s).to eq "Piața Mărăști 3, Cluj-Napoca 400000, Romania"
     expect(Geocoder.kung_foo(46.76267823616486, 23.605241775512695).to_s).to eq "Aleea Muscel 2, Cluj-Napoca 400000, Romania"
