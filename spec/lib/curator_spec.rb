61daf9913306310a010c971bb109e39e2338a6d3
Ionut Muresan <muresan.ionut95@gmail.com>
2015-03-04 12:06:57 +0200

    delete white spacesx

--- spec/lib/curator_spec.rb
+++ spec/lib/curator_spec.rb
@@ -7,7 +7,7 @@ describe Curator do
     it 'has the find_duplicate_coordinates method' do
       expect(curator).to respond_to :find_duplicate_coordinates
     end
-    
+
     it 'returns an array' do
       expect(curator.find_duplicate_coordinates("groapa", 0, 0)).to be_empty
     end
@@ -16,13 +16,13 @@ describe Curator do
       Issue.delete_all
       issue1 = create :issue, category: "category1"
       issue2 = create :issue, lat: issue1.lat, lon: issue1.lon, category: "category1"
-        
+
       result = curator.find_duplicate_coordinates("category1", issue2.lat, issue2.lon)
-      
+
       expect(result).to include(issue1)
       expect(result).to include(issue2)
     end
-    
+
     it 'finds aprox. coordinates' do
       Issue.delete_all
       issue1 = create :issue, lat: 46.772294181082366, lon: 23.59893724322319, category: "category1"
@@ -30,22 +30,22 @@ describe Curator do
       issue3 = create :issue, lat: 46.772294181040000, lon: 23.59893724340000, category: "category1"
       issue4 = create :issue, lat: 46.782294181082366, lon: 23.59893724322319, category: "category1"
       issue5 = create :issue, lat: 46.782294181082366, lon: 23.49893724322319, category: "category1"
-     
-      lat = 46.772294181082366   
+
+      lat = 46.772294181082366
       lon = 23.59893724322319
-      
+
       result = curator.find_duplicate_coordinates("category1", lat, lon);
 
       expect(result).to include(issue1)
       expect(result).to include(issue2)
-      expect(result).to include(issue3) 
+      expect(result).to include(issue3)
       expect(result).to_not include(issue4)
       expect(result).to_not include(issue5)
 
       result = curator.find_duplicate_coordinates("category2", lat, lon);
-      
-      expect(result.size).to eq 0      
-          
+
+      expect(result.size).to eq 0
+
     end
   end
 
@@ -55,4 +55,3 @@ describe Curator do
   context 'overlapping coordinates' do
   end
 end
-   
