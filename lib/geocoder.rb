d2f165493719734cd54dd30b838c0f4f97289bf2
Ionut Muresan <muresan.ionut95@gmail.com>
2015-03-04 11:27:27 +0200

    delete tests for kung_foo method

--- lib/geocoder.rb
+++ lib/geocoder.rb
@@ -2,7 +2,6 @@
 class Geocoder
   include HTTParty
   base_uri 'maps.googleapis.com'
-
   def self.get_info_zone(lat, lon)
     return '' if lat.to_f == 0.0 || lon.to_f == 0.0
     geo = get("/maps/api/geocode/json?latlng=#{lat},#{lon}&sensor=false").body
--- spec/lib/geocoder_spec.rb
+++ spec/lib/geocoder_spec.rb
@@ -2,14 +2,9 @@ require 'spec_helper'
 
 describe Geocoder do
 
-  it 'return info zone' do 
+  it 'return info zone' do
     expect(Geocoder.get_info_zone(46.76714653871027, 23.586959838867188)['results'][0]['address_components'][1]['long_name'].to_s).to eq "Strada Republicii"
-  end
-
-  it 'return locality' do
-    expect(Geocoder.kung_foo(46.777331119287105, 23.614468574523926).to_s).to eq "Piața Mărăști 3, Cluj-Napoca 400000, Romania"
-    expect(Geocoder.kung_foo(46.76267823616486, 23.605241775512695).to_s).to eq "Aleea Muscel 2, Cluj-Napoca 400000, Romania"
-    expect(Geocoder.kung_foo(46.779667569602815, 23.5821533203125).to_s).to eq "Strada Emil Racoviță 59, Cluj-Napoca 400000, Romania"
+    expect(Geocoder.get_info_zone(46.78031411656016, 23.576552867889404)['results'][0]['address_components'][1]['long_name'].to_s).to eq "Gruia"
   end
 
 end
